218547de1f1c5b6b6e25d6d69a56d6ef0bb52109
Tyler Knotek <tekkno89@gmail.com>
2019-02-10 11:29:34 -0600

    fun routes

--- flask_init/hello.py
+++ flask_init/hello.py
@@ -2,6 +2,21 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+  return 'Welcome to the Index'
+
+@app.route('/hey/<name>')
+def say_hey(name):
+  return 'Hey {!s}'.format(name)
+
+@app.route('/number/<int:num>')
+def find_num(num):
+  if num == 42:
+    return 'You got my number!'
+  else:
+    return 'nope, keep guessing'
+
+@app.route('/hiya')
 def hello_world():
   return 'Hiya!'
 
